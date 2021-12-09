6ee46653ac5fb29063b6a28d712798804b109dec
Jibriel Ustarz <jaustarz@cpp.edu>
2021-12-09 21:44:44 +0000

    fixed a line

--- kilo.c
+++ kilo.c
@@ -234,18 +234,18 @@ int getCursorPosition(int *rows, int *cols)
 
     if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
 
-   while(i<sizeof(buf) - 1)
-   {
-       if(read(STDIN_FILENO, &buf[i], 1) != 1) break;
-       if(buf[i] == 'R') break;
-       i++;
-   }
-   buf[i] = '\0';
+    while(i<sizeof(buf) - 1)
+    {
+        if(read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if(buf[i] == 'R') break;
+        i++;
+    }
+    buf[i] = '\0';
 
     if(buf[0] != '\x1b' || buf[1] != '[') return -1;
     if(sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
   
-  return 0;
+    return 0;
 }
 
 int getWindowSize(int *rows, int *cols)
